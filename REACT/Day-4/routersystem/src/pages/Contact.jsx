e72aa9d5754072beac436db86ed51ce0c211c456
surya2819a <suryaarasu1999@gmail.com>
2026-05-19 14:07:52 +0530

    Delete REACT/Day-4 directory

--- REACT/Day-4/routersystem/src/pages/Contact.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Contact = () => {
-  return (
-    <div className='bg-gray-300 p-10 flex justify-center items-center h-screen'>Contact</div>
-  )
-}
-
-export default Contact
